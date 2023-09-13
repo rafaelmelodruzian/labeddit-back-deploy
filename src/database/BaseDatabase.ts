ec60d825ca31354be7e7eb93a800b1d097274664
rafaelmelodruzian <rmdconsultor@gmail.com>
2023-09-13 17:54:41 -0300

    testes executado, iniciando deploy

--- src/database/BaseDatabase.ts
+++ src/database/BaseDatabase.ts
@@ -4,7 +4,7 @@ export abstract class BaseDataBase {
 	protected static connection = knex({
 		client: 'sqlite3',
 		connection: {
-			filename: './src/database/labeddit.db', 
+			filename: './labeddit.db', 
 		},
 		useNullAsDefault: true, 
 		pool: {
--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,9 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.listen(Number(process.env.PORT), () => {
-    console.log(`Servidor rodando na porta ${Number(process.env.PORT)}`)
-})
+app.listen(process.env.PORT || 3003, () => {
+	console.log(`Servidor rodando na porta ${3003}`);
+});
 
 app.use('/users', userRouter);
 app.use('/posts', postRouter);
